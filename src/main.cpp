fdd0b58bf85154ba3f6ea102a5c19a353f4e6886
Erwan CLoux <ecloux@fltechno.scc.com>
2024-11-15 23:32:44 +0100

    add rules

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <fstream>
 
 #include "color.hpp"
 #include "game.hpp"
@@ -11,6 +12,22 @@ int main()
   string name1, name2;
   cout << palette.green << "Bienvenue dans le jeu de dames" << palette.reset << endl;
   cout << palette.green << "================================" << palette.reset << endl;
+  // Read content of rules.txt
+  string line;
+  ifstream rulesFile("rules.txt");
+  if (rulesFile.is_open())
+  {
+    while (getline(rulesFile, line))
+    {
+      cout << line << endl;
+    }
+    rulesFile.close();
+  }
+  else
+  {
+    cout << "Unable to open file";
+  }
+
   cout << palette.red << "Joueur 1, veuillez entrer votre nom" << palette.reset << endl;
   cin >> name1;
   cout << palette.green << "Joueur 2, veuillez entrer votre nom" << palette.reset << endl;
